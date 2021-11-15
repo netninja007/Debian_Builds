135acc84900b502923f1a2273a70960cc73522b5
netninja007 <91462345+netninja007@users.noreply.github.com>
2021-11-15 17:26:42 +0930

    Update keyGenerator.sh

--- SSH/keyGenerator.sh
+++ SSH/keyGenerator.sh
@@ -11,8 +11,8 @@ apt install ssh
 
 #Regenerate the RSA and ED25519 keys
 rm -f /etc/ssh/ssh_host_*
-ssh-keygen -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key -N ""
-ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ""
+ssh-keygen -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key -N " -C $user@$domain
+ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N "" -C $user@$domain
 
 #Enable the RSA and ED25519 keys
 sed -i 's/^\#HostKey \/etc\/ssh\/ssh_host_\(rsa\|ed25519\)_key$/HostKey \/etc\/ssh\/ssh_host_\1_key/g' /etc/ssh/sshd_config
