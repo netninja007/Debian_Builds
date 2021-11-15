134614dc07200e09fc83c71e155a1db82a471202
netninja007 <91462345+netninja007@users.noreply.github.com>
2021-11-15 16:18:53 +0930

    Update keyGenerator.sh

--- SSH/keyGenerator.sh
+++ SSH/keyGenerator.sh
@@ -24,7 +24,10 @@ mv -f /etc/ssh/moduli.safe /etc/ssh/moduli
 #Restrict supported key exchange, cipher, and MAC algorithms
 echo -e "\n# Restrict key exchange, cipher, and MAC algorithms, as per sshaudit.com\n# hardening guide.\nKexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256\nCiphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr\nMACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com\nHostKeyAlgorithms ssh-ed25519,ssh-ed25519-cert-v01@openssh.com,sk-ssh-ed25519@openssh.com,sk-ssh-ed25519-cert-v01@openssh.com,rsa-sha2-256,rsa-sha2-512,rsa-sha2-256-cert-v01@openssh.com,rsa-sha2-512-cert-v01@openssh.com" > /etc/ssh/sshd_config.d/ssh-audit_hardening.conf
 
+#/etc/ssh/sshd_config modifications
 echo "Protocol 2" >> /etc/ssh/sshd_config
+
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 
 #Restart OpenSSH Server
 systemctl restart ssh
