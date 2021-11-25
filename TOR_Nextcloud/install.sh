c09e858b2dd83223625f2d5a2cae4c070728aefa
netninja007 <91462345+netninja007@users.noreply.github.com>
2021-11-25 11:32:10 +0930

    Update install.sh

--- TOR_Nextcloud/install.sh
+++ TOR_Nextcloud/install.sh
@@ -4,7 +4,7 @@
 apt update && apt upgrade
 
 #Install required applications
-apt install wget unzip nginx mariadb-server php7.4 php7.4-fpm php7.4-mysql php-common php7.4-cli php7.4-common php7.4-json php7.4-opcache php7.4-readline php7.4-mbstring php7.4-xml php7.4-gd php7.4-curl php-imagick php7.4-zip php7.4-xml php7.4-bz2 php7.4-intl php7.4-bcmath php7.4-gmp
+apt install wget unzip nginx mariadb-server tor php7.4 php7.4-fpm php7.4-mysql php-common php7.4-cli php7.4-common php7.4-json php7.4-opcache php7.4-readline php7.4-mbstring php7.4-xml php7.4-gd php7.4-curl php-imagick php7.4-zip php7.4-xml php7.4-bz2 php7.4-intl php7.4-bcmath php7.4-gmp
 
 #Start the nginx service.
 systemctl start nginx
@@ -17,9 +17,6 @@ systemctl start mariadb
 
 #Enable the mariadb database on start-up.
 systemctl enable mariadb
-
-#Install the tor service
-apt install tor
 
 #Enable the hidden service
 echo "HiddenServiceDir /var/lib/tor/nextcloud/" >> /etc/tor/torrc
